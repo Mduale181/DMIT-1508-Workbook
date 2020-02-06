b92c970203bbabf4a54ea7eb680b15dee4320dd8
Mduale181 <59575980+Mduale181@users.noreply.github.com>
2020-02-06 13:50:18 -0700

    CHECKING

--- DDL/SchoolTranscript.sql
+++ DDL/SchoolTranscript.sql
@@ -9,7 +9,7 @@
 
 
 /* ==== Create Tables === */
-USE schoolTranscript
+USE SchoolTranscript
 GO
 
 /*=== Drop Statements === */
@@ -27,10 +27,22 @@ CREATE TABLE Students
     StudentID       int             
         CONSTRAINT PK_Students_StudentID            
             PRIMARY KEY 
-        IDENTITY(20200001, 1)            NOT NULL,
-    GivenName      varchar(50)     NOT NULL,
-    Surname         varchar(50)     NOT NULL,
-    DateOfBirth     datetime        NOT NULL,
+        IDENTITY(20200001, 1)			NOT NULL,
+    GivenName      varchar(50)			NOT NULL,
+    Surname         varchar(50)
+		CONSTRAINT CK_Students_Surname
+			CHECK(Surname LIKE ' %')			-- LIKE allows us to do a "pattern-match" of values
+--			CHECK (Surname LIKE '[a-z][a-z]%')	-- two letters plus any other chars
+		--						\ 1\ 1\
+		--Positive match for 'Fred'
+		--Positive match for 'Wu'
+		--Negative match for 'F'
+		--Negative match for '2udor'	
+										NOT NULL,
+    DateOfBirth     datetime			
+		CONSTRAINT CK_Students_DateOfBirth
+			CHECK (DateOfBirth < GETDATE ())
+									NOT NULL,
     Enrolled        bit             
         CONSTRAINT DF_Students_Enrolled
             DEFAULT (1)             NOT NULL
@@ -42,12 +54,22 @@ CREATE TABLE Courses
         CONSTRAINT PK_Courses_Number
             PRIMARY KEY                 NOT NULL,
     [Name]            varchar(50)       NOT NULL,
-    Credits         decimal(3,1)        NOT NULL,
-    [Hours]           tinyint           NOT NULL,
+    Credits         decimal(3,1)        
+		CONSTRAINT CK_Courses_Credits
+		CHECK (Credits > 0 AND Credits <= 6) 
+									NOT NULL,
+    [Hours]           tinyint
+		CONSTRAINT CK_Courses_Hours
+			CHECK ([Hours] BETWEEN 15 AND 180)	-- BETWEEN operator is inclusive
+--			CHECK ([Hours] >= 15 AND [Hours] <=180)
+						           NOT NULL,
     Active          bit                 
     CONSTRAINT  DF_Courses_Active       
             DEFAULT (1)                 NOT NULL,
-    Cost            money               NOT NULL             
+    Cost            money
+		CONSTRAINT Cost
+		CHECK (Cost >= 0)
+			               NOT NULL             
 )
 
 CREATE TABLE StudentCourses
@@ -63,8 +85,46 @@ CREATE TABLE StudentCourses
     [Year]            tinyint           NOT NULL,
     Term           char(3)              NOT NULL,
     FinalMark      tinyint              NULL,
-    [Status]         char(1)            NOT NULL
+    [Status]         char(1)
+		CONSTRAINT CK_StudentCourses_Status
+			CHECK	([Status] =	'E' OR 
+					[Status]  =	'C' OR
+					[Status]  =	'W')
+--			CHECK ([Status] IN ('E', 'C', 'W'))
+							            NOT NULL,
     -- Table-level constraint form comosite keys
     CONSTRAINT PK_StudentCourses_StudentID_CourseNumber
-        PRIMARY KEY (StudentID, CourseNumber)
+        PRIMARY KEY (StudentID, CourseNumber),
+	-- table-level constraint involving more than one column
+	CONSTRAINT CK_StudentCourses_FinalMark_Status
+	CHECK (([Status] = 'C' AND FinalMark IS NOT NULL)
+			OR
+			([Status] IN ('E', 'W') AND FinalMark IS NULL))
 )
+
+
+/*----- Indexes -------*/
+CREATE NONCLUSTERED INDEX IX_StudentCourses_StudentID
+	ON StudentCourses (StudentID)
+
+CREATE NONCLUSTERED INDEX IX_StudentCourses_CourseNumber
+	ON StudentCourses (CourseNumber)
+--For other columns wher searching/sorting might be important
+CREATE NONCLUSTERED INDEX IX_Students_Surname
+	ON Students (Surname)
+
+/* ----- ALTER TABLE statements ------- */
+--1) Add a PostalCode for the Students table
+ALTER TABLE Students
+	ADD PostalCode char(6)	NULL
+	--Adding this as a nullable column, because students already exist, 
+	-- and we don't have postal codes for those students.
+
+GO -- i have to break the above code as a separate batch from the following
+
+-- 2) Make sure the  PostalCode follows the correct pattern A#A#A#
+ALTER TABLE Students
+	ADD CONSTRAINT CK_Students_PostalCode
+		CHECK (PostalCode LIKE '[A-Z][0-9][A-Z][0-9][A-Z][0-9]')
+		-- match for T4R1H2	:	  T	  4	R	1	H		2
+GO
