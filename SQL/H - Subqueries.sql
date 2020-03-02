cf210ea744f04bca9813222f197b552beb357b36
Mduale181 <59575980+Mduale181@users.noreply.github.com>
2020-03-02 11:50:53 -0700

    changes

--- SQL/H - Subqueries.sql
+++ SQL/H - Subqueries.sql
@@ -23,9 +23,17 @@ WHERE  PaymentTypeDescription = 'cash'
 
 --2. Select The Student ID's of all the students that are in the 'Association of Computing Machinery' club
 -- TODO: Student Answer Here
+SELECT StudentID
+FROM   Activity
+WHERE  PaymentTypeID = (SELECT PaymentTypeID
+     FROM   PaymentType
+     WHERE  PaymentTypeDescription = 'cash')
 
 -- 2.b. Select the names of all the students in the 'Association of Computing Machinery' club. Use a subquery for your answer. When you make your answer, ensure the outmost query only uses the Student table in its FROM clause.
-
+SELECT FirstName + ' ' + LastName AS 'Students'
+FROM   Student
+WHERE  StudentID IN
+	(SELECT DISTINCT ClubID FROM Club WHERE ClubName = 'Association of Computing Machinery')
 --3. Select All the staff full names for staff that have taught a course.
 SELECT FirstName + ' ' + LastName AS 'Staff'
 FROM   Staff
@@ -41,7 +49,10 @@ FROM Staff
 
 --4. Select All the staff full names that taught DMIT172.
 -- TODO: Student Answer Here
-
+SELECT FirstName + ' ' + LastName AS 'Staff'
+FROM   Staff
+WHERE  StaffID IN -- I used IN because the subquery returns many rows
+	(SELECT StaffID FROM Registration WHERE CourseId = ( SELECT CourseId FROM Course WHERE CourseId= 'DMIT101' ))
 
 --5. Select All the staff full names of staff that have never taught a course
 SELECT FirstName + ' ' + LastName AS 'Staff'
@@ -110,9 +121,19 @@ WHERE City = 'Edm'
 
 -- 9. What is the avg mark for each of the students from Edm? Display their StudentID and avg(mark)
 -- TODO: Student Answer Here...
+SELECT Student
 
 -- 10. Which student(s) have the highest average mark? Hint - This can only be done by a subquery.
 -- TODO: Student Answer Here...
+SELECT StudentID
+FROM	Registration
+GROUP BY StudentID
+HAVING AVG(Mark) >= ALL --- A number can't be 'GREATER THAN or EQUAL TO' A NULL value
+	(SELECT AVG(Mark)
+	FROM	Registration
+	WHERE	Mark IS NOT NULL 
+	GROUP BY StudentID)
+
 
 -- 11. Which course(s) allow the largest classes? Show the course id, name, and max class size.
 -- TODO: Student Answer Here...
